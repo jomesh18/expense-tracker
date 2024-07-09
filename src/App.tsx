778ecd6142c0815ebc224ed761ff39360ee65be3
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-07-09 18:27:45 +0530

    feat started miniproject

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aa", amount: 10, category: "Utilities" },
+    { id: 2, description: "b", amount: 10, category: "Grocery" },
+    { id: 3, description: "c", amount: 10, category: "Utilities" },
+    { id: 4, description: "d", amount: 10, category: "Utilities" },
+  ]);
+
+  const [data, setData] = useState([
+    {
+      description: "",
+      amount: 0,
+      category: "grocery",
+    },
+  ]);
+
   return (
     <>
-      <Form />
+      <Form
+        sentData={(d) => {
+          setData([...data, d]);
+          console.log(data);
+        }}
+      />
+      <br />
+      <br />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,45 +1,87 @@
-const Form = () => {
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+  description: z
+    .string()
+    .min(1, { message: "Description must be at least 1 character long" })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount should be a number" })
+    .min(0, { message: "Amount should be greater than 0" })
+    .max(10000),
+  category: z
+    .string()
+    .min(1, { message: "Select a valid category from the drop down list" })
+    .max(50),
+});
+
+type FormData = z.infer<typeof schema>;
+
+interface Props {
+  sentData: (data: FormData) => void;
+}
+
+const Form = ({ sentData }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FormData) => {
+    // console.log(data);
+    sentData(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input type="text" id="description" className="form-control" />
+        <input
+          {...register("description")}
+          type="text"
+          id="description"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input type="text" id="amount" className="form-control" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          type="text"
+          id="amount"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
-      <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
           Category
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            <a className="dropdown-item" href="#">
-              Grocery
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Utilities
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Entertainment
-            </a>
-          </li>
-        </ul>
+        </label>
+        <select {...register("category")} id="category" className="form-select">
+          <option value=""></option>
+          <option value="grocery">Grocery</option>
+          <option value="utilities">Utilities</option>
+          <option value="entertainment">Entertainment</option>
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
